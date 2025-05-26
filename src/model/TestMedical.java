dd70413198b9749f7296b31d146a2fe7fc89c296
Bendamou <mohammed.bendamouu@gmail.com>
2025-05-26 19:14:37 +0100

    change structure

--- src/model/TestMedical.java
+++ src/model/TestMedical.java
@@ -12,8 +12,6 @@ public class TestMedical {
     private String typeDoctor;
     private String statut;
 
-    public TestMedical() {}
-
     public TestMedical(int id, int idDoctor, String cniePatient, String typeTest, String resultat, LocalDate dateTest, String typeDoctor, String statut) {
         this.id = id;
         this.idDoctor = idDoctor;
@@ -25,20 +23,71 @@ public class TestMedical {
         this.statut = statut;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public int getIdDoctor() { return idDoctor; }
-    public void setIdDoctor(int idDoctor) { this.idDoctor = idDoctor; }
-    public String getCniePatient() { return cniePatient; }
-    public void setCniePatient(String cniePatient) { this.cniePatient = cniePatient; }
-    public String getTypeTest() { return typeTest; }
-    public void setTypeTest(String typeTest) { this.typeTest = typeTest; }
-    public String getResultat() { return resultat; }
-    public void setResultat(String resultat) { this.resultat = resultat; }
-    public LocalDate getDateTest() { return dateTest; }
-    public void setDateTest(LocalDate dateTest) { this.dateTest = dateTest; }
-    public String getTypeDoctor() { return typeDoctor; }
-    public void setTypeDoctor(String typeDoctor) { this.typeDoctor = typeDoctor; }
-    public String getStatut() { return statut; }
-    public void setStatut(String statut) { this.statut = statut; }
+    public TestMedical() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdDoctor() {
+        return idDoctor;
+    }
+
+    public void setIdDoctor(int idDoctor) {
+        this.idDoctor = idDoctor;
+    }
+
+    public String getCniePatient() {
+        return cniePatient;
+    }
+
+    public void setCniePatient(String cniePatient) {
+        this.cniePatient = cniePatient;
+    }
+
+    public String getTypeTest() {
+        return typeTest;
+    }
+
+    public void setTypeTest(String typeTest) {
+        this.typeTest = typeTest;
+    }
+
+    public String getResultat() {
+        return resultat;
+    }
+
+    public void setResultat(String resultat) {
+        this.resultat = resultat;
+    }
+
+    public LocalDate getDateTest() {
+        return dateTest;
+    }
+
+    public void setDateTest(LocalDate dateTest) {
+        this.dateTest = dateTest;
+    }
+
+    public String getTypeDoctor() {
+        return typeDoctor;
+    }
+
+    public void setTypeDoctor(String typeDoctor) {
+        this.typeDoctor = typeDoctor;
+    }
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+
 }
